b01bf34829c8629c5f73dc908fe803df490be14c
Giuliano Kranevitter <info@giulianok.com>
2017-12-07 19:37:58 -0500

    improving unit testing

--- src/__mocks__/request-promise.js
+++ src/__mocks__/request-promise.js
@@ -1,18 +1,20 @@
 let _ok = false
 let _data
+let _delay
 
 const request = () =>
   new Promise((resolve, reject) => {
     if (_ok) {
-      resolve(_data)
+      if (_delay) setTimeout(() => resolve(_data), _delay)
+      else resolve(_data)
     } else {
       reject()
     }
   })
 
-request.__SET_RESULT = (ok, data) => {
+request.__SET_RESULT = (ok, data, delay) => {
   _ok = ok
-  _data = data
+  ;(_data = data), (_delay = delay)
 }
 
 export default request
